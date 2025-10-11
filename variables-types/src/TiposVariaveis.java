52521506b27229d2480cf80c5b8f0b5ccaa0cbc9
paulosnp <paulocardoso64h@gmail.com>
2025-10-11 02:21:51 -0300

    Modulo de TiposVariaveis

---
+++ variables-types/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        String meuNome = "Paulo Cardoso";
+
+        double salarioMinino = 1518;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+
+}
